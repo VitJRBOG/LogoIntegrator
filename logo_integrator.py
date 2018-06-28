692819066426c6c8db705c37509462418a0c14b2
Vitaly Bogryakov <vitjrbog@gmail.com>
2018-06-28 10:16:29 +0500

    Update
    - удалены изменения, которые не применялись в продакшене и о которых я ничего не помню;
    - изменен принцип добавления логотипа: теперь это текст, который скрипт рисует сам, а не картинка из внешнего файла.

--- logo_integrator.py
+++ logo_integrator.py
@@ -2,144 +2,146 @@
 
 
 import os
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 
 def starter():
-    try:
-        if os.path.exists("path.txt") is False:
-            file_text = open("path.txt", "w")
-            file_text.write("")
-            file_text.close()
-            print("\nCOMPUTER: Was created file \"path.txt\".")
 
-        PATH = read_txt("path.txt")
+    if os.path.exists("path.txt") is False:
+        file_text = open("path.txt", "w")
+        file_text.write("")
+        file_text.close()
+        print("\nCOMPUTER: Was created file \"path.txt\".")
 
-        if os.path.exists("logo.txt") is False:
-            file_text = open("logo.txt", "w")
-            file_text.write("")
-            file_text.close()
-            print("\nCOMPUTER: Was created file \"logo.txt\".")
+    PATH = read_txt("path.txt")
 
-        LOGO = read_txt("logo.txt")
+    if os.path.exists("logo.txt") is False:
+        file_text = open("logo.txt", "w")
+        file_text.write("")
+        file_text.close()
+        print("\nCOMPUTER: Was created file \"logo.txt\".")
 
-        if os.path.exists(LOGO + "logo") is False:
-            os.mkdir(str(LOGO) + "logo")
-            print("\nCOMPUTER: Was created directory \"logo\".")
+    LOGO = read_txt("logo.txt")
 
-        if os.path.exists(PATH + "images") is False:
-            os.mkdir(str(PATH) + "images")
-            print("\nCOMPUTER: Was created directory \"images\".")
+    if os.path.exists(LOGO + "logo") is False:
+        os.mkdir(str(LOGO) + "logo")
+        print("\nCOMPUTER: Was created directory \"logo\".")
 
-        if os.path.exists(PATH + "output") is False:
-            os.mkdir(str(PATH) + "output")
-            print("\nCOMPUTER: Was created directory \"output\".")
+    if os.path.exists(PATH + "images") is False:
+        os.mkdir(str(PATH) + "images")
+        print("\nCOMPUTER: Was created directory \"images\".")
 
-    except Exception as var_except:
-        print("\nCOMPUTER: Error, " +
-              str(var_except) +
-              ". Exit from program...")
-        exit()
+    if os.path.exists(PATH + "output") is False:
+        os.mkdir(str(PATH) + "output")
+        print("\nCOMPUTER: Was created directory \"output\".")
 
     main_menu()
 
 
 def read_txt(file_name):
-    try:
-        path = str(open(file_name, "r").read())
 
-        if len(path) > 0 and path[len(path) - 1] != "/":
-            path += "/"
+    path = str(open(file_name, "r").read())
 
-        return path
+    if len(path) > 0 and path[len(path) - 1] != "/":
+        path += "/"
 
-    except Exception as var_except:
-        print("\nCOMPUTER [.. -> Read " + file_name + "]: Error, " +
-              str(var_except) +
-              ". Return to Main menu...")
+    return path
 
     main_menu()
 
 
 def main_menu():
 
-    try:
-        print("\nCOMPUTER [Main menu]: You are in Main menu.")
-        print("COMPUTER [Main menu]: 1 == Integrate a logo to images.")
-        print("COMPUTER [Main menu]: 0 == Close program.")
+    print("\nCOMPUTER [Main menu]: You are in Main menu.")
+    print("COMPUTER [Main menu]: 1 == Integrate a logo to images.")
+    print("COMPUTER [Main menu]: 0 == Close program.")
 
-        user_answer = raw_input("USER [Main menu]: (1/0) ")
+    user_answer = raw_input("USER [Main menu]: (1/0) ")
 
-        if user_answer == "0":
-            print("COMPUTER [Main menu]: Exit from program...")
-            exit()
-        else:
-            if user_answer == "1":
-                logo_integrate()
-            else:
-                print("\nCOMPUTER [Main menu]: Unknown command. " +
-                      "Retry query...")
-                main_menu()
-
-    except Exception as var_except:
-        print("\nCOMPUTER [Main menu]: Error, " +
-              str(var_except) +
-              ". Exit from program...")
+    if user_answer == "0":
+        print("COMPUTER [Main menu]: Exit from program...")
         exit()
+    else:
+        if user_answer == "1":
+            logo_integrate()
+        else:
+            print("\nCOMPUTER [Main menu]: Unknown command. " +
+                  "Retry query...")
+            main_menu()
 
 
 def logo_integrate():
 
-    try:
-        PATH = read_txt("path.txt")
-        LOGO = read_txt("logo.txt")
+    def get_logo_image(text, width_target, height_target):
+        def get_image(text, font_size, color, font_path, width_target, height_target):
+            image_font = ImageFont.truetype(font_path, font_size)
+            height_image = sum(image_font.getmetrics())
+            width_image = image_font.getsize(text)[0]
+            objImage = Image.new('L', (width_image, height_image))
+            ImageDraw.Draw(objImage).text((0, 0), text, fill=100, font=image_font)
+            objImage = objImage.rotate(45, resample=Image.BICUBIC, expand=True)
 
-        list_files_images = os.listdir(PATH + "images/")
+            return objImage
 
-        if len(list_files_images) < 1:
-            print("\nCOMPUTER [.. Integrate a logo to images]: " +
-                  "Folder \"images\" is empty. " +
-                  "Please, copy images and try again. " +
-                  "Return to Main menu...")
-            main_menu()
+        color = (255, 255, 255)
+        font_path = "/home/vitjrbog/.fonts/my/impact.ttf"
+        if int(width_target) > int(height_target):
+            font_size = int(0.012 * int(width_target)) + 2
+        else:
+            font_size = int(0.020 * int(height_target)) + 2
+        objLogoImage = get_image(text, font_size, color, font_path, width_target, height_target)
 
-        i = 0
-        while i < len(list_files_images):
-            file_name_image = list_files_images[i]
+        return objLogoImage
 
-            image = Image.open(PATH + "images/" + file_name_image)
-            logo = Image.open(LOGO + "logo/logo.png")
+    PATH = read_txt("path.txt")
 
-            (width_image, height_image) = image.size
-            (width_logo, height_logo) = logo.size
+    list_files_images = os.listdir(PATH + "images/")
 
-            x = 5
-            while x < int(width_image):
-                y = 5
-                while y < int(height_image):
+    if len(list_files_images) < 1:
+        print("\nCOMPUTER [.. Integrate a logo to images]: " +
+              "Folder \"images\" is empty. " +
+              "Please, copy images and try again. " +
+              "Return to Main menu...")
+        main_menu()
 
-                    image.paste(logo, (x, y), logo)
+    logo = "vk.com/orb_overhear"
+    i = 0
+    while i < len(list_files_images):
+        file_name_image = list_files_images[i]
 
-                    y += int(height_logo) + 10
+        objTargetImage = Image.open(PATH + "images/" + file_name_image)
+        (width_image, height_image) = objTargetImage.size
 
-                x += int(width_logo) + 10
+        if int(width_image) > int(height_image):
+            width_interval = int(width_image * 0.03)
+            height_interval = int(height_image * 0.12)
+        else:
+            width_interval = int(width_image * 0.03)
+            height_interval = int(height_image * 0.12)
 
-            image.save(PATH + "output/watermarked_" + file_name_image)
-            print("\nCOMPUTER [.. -> Integrate a logo to images]: " +
-                  "Logo was successfully integrated to \"" +
-                  str(file_name_image) + "\".")
+        objLogoImage = get_logo_image(logo, width_image, height_image)
+        (width_logo, height_logo) = objLogoImage.size
 
-            i += 1
+        x = width_interval
+        while x < int(width_image):
+            y = int(height_interval / 4)
+            while y < int(height_image):
 
-        logo.close()
+                objTargetImage.paste((255, 255, 255), (x, y), objLogoImage)
 
+                y += int(height_logo) + height_interval
+
+            x += int(width_logo) + width_interval
+
+        objTargetImage.save(PATH + "output/watermarked_" + file_name_image)
         print("\nCOMPUTER [.. -> Integrate a logo to images]: " +
-              "All images was changed. Return to Main menu.")
+              "Logo was successfully integrated to \"" +
+              str(file_name_image) + "\".")
 
-    except Exception as var_except:
-        print("\nCOMPUTER [.. -> Integrate a logo to images]: Error, " +
-              str(var_except) +
-              ". Return to Main menu...")
+        i += 1
+
+    print("\nCOMPUTER [.. -> Integrate a logo to images]: " +
+          "All images was changed. Return to Main menu.")
 
     main_menu()
 
