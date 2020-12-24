f58122753a0590e668cec6b8b4b61be7b9026caf
VitJRBOG <vitjrbog@icloud.com>
2020-12-24 09:52:19 +0500

    Refactor - был создан пакет filemanager для работы с файлами: функции, выполняющие операции с файлами перемещены из главного пакета в filemanager; - был создан пакет imgs для работы с изображениями: функция AddLogoToImage(), выполняющая операции с изображениями, перемещена из главного пакета в imgs; - функция findFilesImages() была переименована в GetImageFileNames() и разбита на две меньшие функции - findFileNames() и selectImageFileNames(); - функции openImage() и saveImage() пакета filemanager сделаны публичными; - функция addLogoToImage() пакета imgs сделана публичной и разбита на две меньшие функции - makeNewImage и checkNewOverlayPosition; - добавлены комментарии с публичным функциям.

--- main.go
+++ main.go
@@ -1,95 +1,17 @@
 package main
 
 import (
-	"image"
-	"image/draw"
-	"image/png"
-	"io/ioutil"
-	"os"
-	"strings"
+	"github.com/VitJRBOG/test_go_logo_integrator/filemanager"
+	"github.com/VitJRBOG/test_go_logo_integrator/imgs"
 )
 
 func main() {
-	createDirectories()
-	filesNames := findFilesImages("images/")
-	for _, fileName := range filesNames {
-		origImage := openImage("images/" + fileName)
-		logoImage := openImage("logo/logo.png")
-		newImage := addLogoToImage(origImage, logoImage)
-		saveImage(newImage, fileName)
+	filemanager.CreateDirectories()
+	fileNames := filemanager.GetImageFileNames("images/")
+	for _, fileName := range fileNames {
+		origImage := filemanager.OpenImage("images/" + fileName)
+		logoImage := filemanager.OpenImage("logo/logo.png")
+		imageWithLogo := imgs.AddLogoToImage(origImage, logoImage)
+		filemanager.SaveImage(imageWithLogo, fileName)
 	}
-}
-
-func createDirectories() {
-	os.Mkdir("images", 0700)
-	os.Mkdir("output", 0700)
-}
-
-func findFilesImages(path string) []string {
-	var filesNames []string
-	files, err := ioutil.ReadDir(path)
-	if err != nil {
-		panic(err.Error())
-	}
-	for _, file := range files {
-		posFileExtension := strings.LastIndex(file.Name(), ".") + 1
-		fileExtension := file.Name()[posFileExtension:]
-		if fileExtension == "png" {
-			filesNames = append(filesNames, file.Name())
-		}
-	}
-	return filesNames
-}
-
-func openImage(path string) image.Image {
-	fileImage, err := os.Open(path)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer fileImage.Close()
-	image, err := png.Decode(fileImage)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	return image
-}
-
-func addLogoToImage(origImage, logoImage image.Image) *image.RGBA {
-
-	newImage := image.NewRGBA(origImage.Bounds())
-
-	draw.Draw(newImage, origImage.Bounds(), origImage, image.Point{0, 0}, draw.Src)
-
-	y := logoImage.Bounds().Max.Y
-	for {
-		x := logoImage.Bounds().Max.X
-		for {
-			draw.Draw(newImage, origImage.Bounds(), logoImage, image.Point{x, y}, draw.Over)
-
-			x -= logoImage.Bounds().Max.X
-
-			if -(x) >= newImage.Bounds().Max.X {
-				break
-			}
-		}
-
-		y -= logoImage.Bounds().Max.Y
-
-		if -(y) >= newImage.Bounds().Max.Y {
-			break
-		}
-	}
-
-	return newImage
-}
-
-func saveImage(newImage *image.RGBA, fileName string) {
-	newImageFile, err := os.Create("output/" + fileName)
-	if err != nil {
-		panic(err.Error())
-	}
-	defer newImageFile.Close()
-
-	png.Encode(newImageFile, newImage)
 }
